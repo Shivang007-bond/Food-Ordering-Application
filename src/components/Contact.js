dd35c641f2f4b182832ac29fcf72cc65e3271e80
Shivang Tripathi <77275400+Shivang007-bond@users.noreply.github.com>
2023-10-02 12:13:50 +0530

    contact page added demo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,25 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="about-cntnr">
+      <div className="data-cntnr">
+        <p className="data">
+          <h3>Speak to us : +91 - 9140743947</h3>
+          <h5>
+            To know more visit -
+            <a href="https://www.linkedin.com/in/shivang-tripathi-">
+              {" "}
+              Linkedin/Shivang Tripathi
+            </a>
+          </h5>
+          <h5>
+            Github -{" "}
+            <a href="https://github.com/Shivang007-bond">
+              {" "}
+              Github/Shivang Tripathi
+            </a>
+          </h5>
+        </p>
+      </div>
     </div>
   );
 };
